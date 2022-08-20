7c3d52612bbb0b165fa823a699e3b65456f27ef2
metapush89 <111631650+metapush89@users.noreply.github.com>
2022-08-20 11:58:47 -0400

    Update dockerImg.py

--- dockerImg.py
+++ dockerImg.py
@@ -20,8 +20,6 @@ FROM {i}
 RUN echo 'Hello World'
         """)
 
-    os.system(f'cat {os.path.join(os.getcwd(), "images", randomString)}')  
-
     # generate random string
     os.system(f"docker build -t {randomString} -f ./images/{randomString} ./images")
     os.system("docker system prune --all --force")
