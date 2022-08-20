a853d5e9c29cc55636da557952fefa2908fe1f2a
Your Name <you@example.com>
2022-08-20 04:22:57 -0400

    fixed

--- herokuImg.py
+++ herokuImg.py
@@ -1,4 +1,6 @@
+import random, os
 
+content = f"""
 # Generate a basic flask application
 import os
 from flask import Flask
@@ -7,9 +9,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World! - 704</h1>'
+    return '<h1>Hello World! - {random.randint(1, 1000)}</h1>'
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5210))
 
+"""
+
+with open(os.path.join(os.getcwd(), 'server', 'app.py'), 'w') as f:
+    f.write(content)
