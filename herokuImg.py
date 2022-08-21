951a0ddb8598b56262ef5d58605ec781d17a2577
Your Name <you@example.com>
2022-08-21 02:53:47 -0400

    herok

--- herokuImg.py
+++ herokuImg.py
@@ -14,7 +14,6 @@ def trtst():
     FF_profile = webdriver.FirefoxProfile()
     FF_options.add_argument("-headless")
 	# enable trace level for debugging 
-	FF_options.log.level = "trace"
 	FF_options.add_argument("-remote-debugging-port=9224")
 	FF_options.add_argument("-headless")
 	FF_options.add_argument("-disable-gpu")
