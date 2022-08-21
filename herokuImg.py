329267b04f6ae98243e93c0d2f3506d7612fecc6
Your Name <you@example.com>
2022-08-21 02:37:35 -0400

    sele

--- herokuImg.py
+++ herokuImg.py
@@ -2,18 +2,33 @@ import random, os
 
 content = f"""
 import os
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/trtst')
+def trtst():
+    FF_options = webdriver.FirefoxOptions()
+    FF_profile = webdriver.FirefoxProfile()
+    FF_options.add_argument("-headless")
+    if 'DYNO' in os.environ:
+        driver = webdriver.Firefox(executable_path=os.environ.get("GECKODRIVER_PATH"),firefox_binary=FirefoxBinary(os.environ.get("FIREFOX_BIN"))) 
+    else:
+        driver = webdriver.Firefox(options=FF_options, firefox_profile=FF_profile)
+    driver.get("http://info.cern.ch/hypertext/WWW/TheProject.html")
+    textResult = driver.execute_script('return document.querySelector("title").textContent')
+    driver.close()
+    textMatch = 'The World Wide Web project'
+    return str(textResult == textMatch)
 
 @app.route('/')
 def index():
     return '<h1>Hello World! - {random.randint(1, 1000)}</h1>'
 
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5210))
-
 """
 
 with open(os.path.join(os.getcwd(), 'server', 'app.py'), 'w') as f:
